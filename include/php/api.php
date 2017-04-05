075904e251ae3026c4b8503f172cca949f6a56a1
Dmytro Vasylenko <necronth@gmail.com>
2017-04-05 17:01:01 +0300

    fix multi-places bug

--- include/php/api.php
+++ include/php/api.php
@@ -9,8 +9,12 @@
 		case "add":
 			$queueId = $_POST["queue_id"];
 			$place = $_POST["place"];
-			mysqli_query($mysqli, "INSERT INTO places(`id`, `user`, `queue_id`, `place`)
-				VALUES('', '".$_COOKIE["uid"]."', '".$queueId."', '".$place."')");
+			$exist = mysqli_query($mysqli, "SELECT * FROM places WHERE queue_id='".$queueId."' AND (place='".$place."' OR user='".$_COOKIE["uid"]."')");
+			if(!mysqli_num_rows($exist))
+			{
+				mysqli_query($mysqli, "INSERT INTO places(`id`, `user`, `queue_id`, `place`)
+					VALUES('', '".$_COOKIE["uid"]."', '".$queueId."', '".$place."')");
+			}
 		break;
 	}
 ?>
