7996197d91db8f84e9f32357c57f9e4bbcb2bba5
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-09-05 07:43:40 +0300

    fix duplication, add email and move to es6

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ const http = require("http").Server(server);
 const database = require("./database");
 const ws = require("./ws")(http);
 
+const PORT = 3001;
 
-server.use(function(req, res, next) {
+server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', req.headers.origin);
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
@@ -14,29 +15,33 @@ server.use(function(req, res, next) {
 server.use(bodyPareser.json());
 server.use(bodyPareser.urlencoded({extended: false}));
 
-server.post("/api/place/", function(res, req) {
+server.post("/api/place/", (res, req) => {
+	let {id, place, name, photo, email} = res.body;
+
 	database.addPlace({
-		id: Number(res.body.id),
-		place: res.body.place,
-		name: res.body.name
+		id: Number(id),
+		place,
+		name,
+		photo,
+		email
 	}, function(err) {
 		if(err) {
 			return req.status(500).send(err);
 		}
 		ws.socketBroadcast("NEW_PLACE", {
-			id: res.body.id,
-			place: res.body.place,
-			placeInfo: {
-				img: "man.png",
-				name: "Dmitry"
+			id,
+			place,
+			placeData: {
+				photo,
+				name
 			}
 		});
 		return req.sendStatus(200);
 	});
 });
 
-server.get("/api/queues/", function(res, req) {
-	database.getQueues(function(err, queues) {
+server.get("/api/queues/", (res, req) => {
+	database.getQueues((err, queues) => {
 		if(err) {
 			return req.status(500).send(err);
 		}
@@ -44,6 +49,6 @@ server.get("/api/queues/", function(res, req) {
 	});
 });
 
-http.listen(3001, function() {
-	console.log("Server started at port 3001");
+http.listen(PORT, () => {
+	console.log("Server started at port", PORT);
 });
