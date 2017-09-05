681ea140370ea294bf7ed04809f68e51acdc4c3b
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-09-05 06:26:20 +0300

    do refactoring

--- client/src/redux/Reducer.jsx
+++ client/src/redux/Reducer.jsx
@@ -6,20 +6,21 @@ const initialState = {
 };
 
 const reducer = function(state = initialState, action) {
-	if(action.type === SET_QUEUE) {
-		var places = {};
-		action.payload.places.forEach(item => {
-			places[item.place] = item.name;
-		});
-		action.payload.places = places;
-		state.queues[action.payload.id] = action.payload;
-		return state;
-	} else if(action.type === SET_PLACE) {
-		var newQueues = state.queues;
-		newQueues[action.payload.id].places[action.payload.place] = action.payload.placeInfo;
-		return {
-			queues: newQueues
-		};
+	switch(action.type) {
+		case SET_QUEUE:
+			var places = {};
+			action.payload.places.forEach(item => {
+				places[item.place] = item.name;
+			});
+			action.payload.places = places;
+			state.queues[action.payload.id] = action.payload;
+			return state;
+		case SET_PLACE:
+			var newQueues = state.queues;
+			newQueues[action.payload.id].places[action.payload.place] = action.payload.placeInfo;
+			return {
+				queues: newQueues
+			};
 	}
 }
 
