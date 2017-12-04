f0a7a1dad0b887f23f1b433ace71e870ecad642d
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-12-04 17:51:47 +0200

    remove props.selectedPlace

--- client/src/containers/Place.jsx
+++ client/src/containers/Place.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Place = props => (
-    <div className={props.selected ? "place selected-place" : "place"} data-id={props.id}>
+    <div className="place" data-id={props.id}>
         <img src={props.photo} alt={props.name} title={props.name} />
     </div>
 );
