9d832ef8ad00770ee0ab0757655f2ccdf25a0b2a
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-12-04 17:50:54 +0200

    create new styles' file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import React, {Component} from "react";
 
-import "./style/App.css";
+import "./style/App.min.css";
 import Content from "./Content";
 import Header from "./components/Header";
-// import WS from "./websocket/WebSocket";
+import WS from "./websocket/WebSocket";
 
 class App extends Component {
     render() {
         return (
             <div id="wrapper">
-                {/*<WS />*/}
+                <WS />
                 <Header />
                 <Content />
             </div>
