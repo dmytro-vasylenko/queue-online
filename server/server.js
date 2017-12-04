5880f95462a8619e1b8822d6e75408b46c910aee
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-12-04 17:48:52 +0200

    add Access-Control-Allow-Methods

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ server.use((req, res, next) => {
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
     next();
 });
 server.use(bodyPareser.json());
