cfca8e562c44d98b94f2f717c9f208684c31a9ec
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-12-04 17:51:26 +0200

    fix constants

--- client/src/websocket/WebSocket.jsx
+++ client/src/websocket/WebSocket.jsx
@@ -3,17 +3,14 @@ import {connect} from "react-redux";
 import openSocket from "socket.io-client";
 
 import {SOCKET} from "../constants/config";
-
-const NEW_PLACE = "NEW_PLACE";
-const REMOVE_PLACE = "REMOVE_PLACE";
-const NEW_QUEUE = "NEW_QUEUE";
+import {ADD_PLACE, REMOVE_PLACE, ADD_QUEUE} from "../constants/types";
 
 class WS extends Component {
     io = openSocket(SOCKET);
     componentDidMount() {
-        this.io.on(NEW_PLACE, data => this.props.onSetPlace(data));
-        this.io.on(REMOVE_PLACE, data => this.props.onRemovePlace(data));
-        this.io.on(NEW_QUEUE, data => this.props.onNewQueue(data));
+        this.io.on("NEW_PLACE", data => this.props.onSetPlace(data));
+        this.io.on("REMOVE_PLACE", data => this.props.onRemovePlace(data));
+        this.io.on("NEW_QUEUE", data => this.props.onNewQueue(data));
     }
 
     render() {
@@ -24,13 +21,13 @@ class WS extends Component {
 const mapDispatchToProps = dispatch => {
     return {
         onSetPlace: data => {
-            dispatch({type: "SET_PLACE", payload: data});
+            dispatch({type: ADD_PLACE, payload: data});
         },
         onRemovePlace: data => {
-            dispatch({type: "REMOVE_PLACE", payload: data});
+            dispatch({type: REMOVE_PLACE, payload: data});
         },
         onNewQueue: data => {
-            dispatch({type: "SET_QUEUE", payload: data});
+            dispatch({type: ADD_QUEUE, payload: data});
         }
     };
 };
