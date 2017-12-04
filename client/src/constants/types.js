30516b0dbfa403fb630abf1c9a67a9d727498fc7
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-12-04 17:52:53 +0200

    fix constant

--- client/src/constants/types.js
+++ client/src/constants/types.js
@@ -1,5 +1,5 @@
 export const ADD_QUEUE = "ADD_QUEUE";
-export const SET_PLACE = "SET_PLACE";
+export const ADD_PLACE = "ADD_PLACE";
 export const REMOVE_PLACE = "REMOVE_PLACE";
 export const NEW_QUEUE = "NEW_QUEUE";
 export const UPDATE_QUEUES = "UPDATE_QUEUES";
