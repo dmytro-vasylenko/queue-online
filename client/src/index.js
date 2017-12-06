63e4e384b727f88f446a9cab7a8a1e97034c5586
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-12-06 04:53:41 +0200

    refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,18 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {Router, Route, hashHistory} from "react-router";
 
-import reducer from "./redux/Reducer";
+import reducer from "./store";
 import App from "./App";
 import Admin from "./Admin";
 
-const store = createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
-			<Route path="/" component={App} />
-			<Route path="/admin" component={Admin} />
-		</Router>
-	</Provider>,
-	document.getElementById("root")
+    <Provider store={store}>
+        <Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
+            <Route path="/" component={App} />
+            <Route path="/admin" component={Admin} />
+        </Router>
+    </Provider>,
+    document.getElementById("root")
 );
